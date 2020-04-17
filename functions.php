9eefac528fd0c7f4cda94cabe63bafc80c570a8b
r3dfoxx <jekagrabar@gmail.com>
2020-04-17 21:07:56 +0300

    new quantity

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@
         $products[] = ["id" => 2, "name" => "LG TV", "price" => 13000, "quantity" => 8];
         $products[] = ["id" => 3, "name" => "Iphone XS", "price" => 13000, "quantity" => 8];
         $products[] = ["id" => 4, "name" => "Xbox", "price" => 10000, "quantity" => 10];
-        $products[] = ["id" => 5, "name" => "PHP Programming", "price" => 1000, "quantity" => 2];
+        $products[] = ["id" => 5, "name" => "PHP Programming", "price" => 1000, "quantity" => 10];
         file_put_contents("products.json", json_encode($products));
     }
